0e2ad9c6937f528bb122d3f7548d8f1c6b2982e2
Max Papush <60475350+Maxobus@users.noreply.github.com>
2025-07-20 11:33:08 +0300

    Create run.sh

---
+++ frp-client/run.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bashio
+WAIT_PIDS=()
+CONFIG_PATH='/share/frpc.toml'
+
+function stop_frpc() {
+    bashio::log.info "Shutdown frpc client"
+    kill -15 "${WAIT_PIDS[@]}"
+}
+
+bashio::log.info "Generating frpc.toml from UI config"
+
+cat <<EOF > $CONFIG_PATH
+serverAddr = "$(bashio::config 'serverAddr')"
+serverPort = $(bashio::config 'serverPort')
+auth.method = "token"
+auth.token = "$(bashio::config 'authToken')"
+
+log.to = "/share/frpc.log"
+log.level = "trace"
+log.maxDays = 3
+
+webServer.addr = "0.0.0.0"
+webServer.port = $(bashio::config 'webServerPort')
+webServer.user = "$(bashio::config 'webServerUser')"
+webServer.password = "$(bashio::config 'webServerPassword')"
+
+[[proxies]]
+name = "$(bashio::config 'proxyName')"
+type = "tcp"
+transport.useEncryption = true
+transport.useCompression = true
+remotePort = $(bashio::config 'remotePort')
+localPort = 8123
+localIP = "0.0.0.0"
+EOF
+
+bashio::log.info "Starting frp client"
+cat $CONFIG_PATH
+
+cd /usr/src
+./frpc -c $CONFIG_PATH & WAIT_PIDS+=($!)
+tail -f /share/frpc.log &
+
+trap "stop_frpc" SIGTERM SIGHUP
+wait "${WAIT_PIDS[@]}"
